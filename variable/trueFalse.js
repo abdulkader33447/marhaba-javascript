a95fc951f2145cf7757b4ddac0a94cb829a87cfe
abdulkader33447 <abdulkader33447@gmail.com>
2025-05-11 17:43:26 +0600

    added true false

---
+++ variable/trueFalse.js
@@ -0,0 +1,25 @@
+const eatBamboo= true
+console.log("eat Bamboo ?",eatBamboo);
+
+const isMessage= false
+console.log("she send message ?",isMessage);
+
+const isCelebrating = true
+console.log("celebrating now ?",isCelebrating);
+
+const isHappy=true;
+const hasMoney=false;
+const isHungry=true;
+
+console.log("are you happy?",isHappy)
+console.log("you have mony?",hasMoney)
+console.log("are hungry?",isHungry)
+
+const isStudyDone =true
+console.log("study done?",isStudyDone);
+
+const isPhoneCharged= true
+console.log("battery full?",isPhoneCharged);
+
+const isDinnerDone = false
+console.log("Dinner done?",isDinnerDone);
