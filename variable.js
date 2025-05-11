fef76fb275c635c6a6d983f01b8b10fd85b0a231
abdulkader33447 <abdulkader33447@gmail.com>
2025-05-11 16:22:59 +0600

    added what is variable

--- variable.js
+++ variable.js
@@ -1,1 +1,20 @@
-const bornYer = 1999;
+const bornYear = 1999;
+console.log("bornYear", bornYear);
+
+const siblings = 5;
+console.log("siblings", siblings);
+
+const studentsMyClass = 4000;
+console.log("students My Class", studentsMyClass);
+
+const lastResult = 59;
+console.log("las result", lastResult);
+
+const roomsMyFlat = 3;
+console.log("rooms my flat", roomsMyFlat);
+
+const readTime = 10;
+console.log("read time",readTime);
+
+const total = 50
+console.log("total",total);
