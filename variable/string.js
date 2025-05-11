c7fa50f780bfba3fdba2841c0a6a3270032ce1c7
abdulkader33447 <abdulkader33447@gmail.com>
2025-05-11 17:01:55 +0600

    added string

---
+++ variable/string.js
@@ -0,0 +1,20 @@
+const name = "abdul kader";
+console.log("name:", name);
+
+const address = "tangail";
+console.log("address:", address);
+
+const school = "programming hero";
+console.log("school:", school);
+
+const fan = "bala";
+console.log("actor:", fan);
+
+const movieOfTheYear = "mufasa";
+console.log("movie of the year:", movieOfTheYear);
+
+const hobby = "slipping";
+console.log("hobby", hobby);
+
+const color = "sky blue";
+console.log("favorite color :", color);
