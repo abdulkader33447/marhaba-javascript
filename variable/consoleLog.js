ef5bd667d44e482d87fbb9bd0b8b09192f934901
abdulkader33447 <abdulkader33447@gmail.com>
2025-05-11 16:43:38 +0600

    added console

---
+++ variable/consoleLog.js
@@ -0,0 +1,20 @@
+const age = 26;
+console.log("age",age);
+
+const bookToRead = 27;
+console.log("book To read", bookToRead);
+
+const wakeUpTime = 8;
+console.log("wake up time",wakeUpTime);
+
+const wastedTime = 3
+console.log("wasted time in phone",wakeUpTime);
+
+const borrow = 500
+console.log("loan",borrow);
+
+const goals = 0 
+console.log("goal",0);
+
+const pillow = 1
+console.log("pillow",pillow);
