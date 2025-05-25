a7d44fd74555729cde8795ce238942c4328846b5
abdulkader33447 <abdulkader33447@gmail.com>
2025-05-25 18:13:48 +0600

    added math file

---
+++ variable/math.js
@@ -0,0 +1,45 @@
+const eggPrice = 10;
+const onionPrice = 20;
+const total = eggPrice + onionPrice;
+console.log(total);
+// console.log(eggPrice + onionPrice);
+
+const eggiPrice = 15;
+const onnionPrice = 10;
+const result = eggiPrice - onnionPrice;
+console.log(result);
+// console.log(eggiPrice - onnionPrice); //✅
+
+const carrotPrice = 5;
+const tomatoPrice = 2;
+const multiplication = carrotPrice * tomatoPrice;
+console.log(multiplication);
+// console.log(carrotPrice * tomatoPrice); //✅
+
+const potato = 20;
+const apple = 50;
+// const div = potato / apple; //✅
+const division = apple / potato;
+console.log(division);
+// console.log(potato / apple); //✅
+// console.log(apple/potato); //✅
+
+// console.log(10 % 3); //✅
+const alu = 25;
+const vag = 5;
+const modules = alu % vag;
+console.log(modules);
+
+const first = "abdul";
+const second = "kader";
+const add = first + second;
+console.log(add);
+// console.log(first + second); //✅
+
+const price= 10
+const indexPrice = price+10
+// console.log(indexPrice);
+const newPrice = price+30
+const discountedPrice = newPrice-10
+console.log(discountedPrice);
+
