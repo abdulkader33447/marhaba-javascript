e46d7514fdc999c712124ddf747ad3c955f0de9d
abdulkader33447 <abdulkader33447@gmail.com>
2025-05-26 17:46:45 +0600

    finish math chapter

--- variable/math.js
+++ variable/math.js
@@ -36,10 +36,61 @@ const add = first + second;
 console.log(add);
 // console.log(first + second); //✅
 
-const price= 10
-const indexPrice = price+10
+const price = 10;
+const indexPrice = price + 10;
 // console.log(indexPrice);
-const newPrice = price+30
-const discountedPrice = newPrice-10
+const newPrice = price + 30;
+const discountedPrice = newPrice - 10;
 console.log(discountedPrice);
 
+// ------practice
+const earn = 100;
+const snacks = 40;
+// console.log(earn-snacks); //✅
+const mines = earn - snacks;
+console.log(mines);
+
+const pencil = 10;
+const use = 2;
+console.log(pencil / use, "bar use korte parbo");
+
+const iHave = 60;
+const used = 15;
+console.log(iHave / used, "bar khoroch korte parbo");
+console.log(iHave % used, "taka thakbe");
+
+const firstWord = "Hello";
+const secondWord = "World";
+const adding = firstWord + secondWord;
+console.log(adding);
+// console.log(firstWord + secondWord); //✅
+
+const chal = 153;
+const dal = 261;
+const totalBazar = chal + dal;
+console.log("total weight", totalBazar, "kg");
+
+const taka = 500;
+const usePar = 75;
+console.log(parseInt(taka / usePar), "bar khoroch korte parbo");
+console.log(taka % usePar, "taka thakbe");
+
+const four = 4;
+const three = 3;
+console.log("vagfol", four / three);
+console.log("vagsesh", four % three);
+
+const fifty = 50;
+const nine = 9;
+console.log(fifty % nine);
+
+const bangla = "bangla";
+const desh = "desh";
+console.log(bangla + desh);
+
+const chall=480
+const char=4
+const din=chall/char
+console.log(din,"din khete parbi");
+const mas=30
+console.log(din/mas ,"mas khete parbi");
